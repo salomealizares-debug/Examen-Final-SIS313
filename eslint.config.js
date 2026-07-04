8a7bec80e6bb32c1f1f81998bf1082fcc8ebbb6c
Nerida Ordoñez <vivianordonezchambi@gmail.com>
2026-07-04 04:00:17 -0400

    feat: adding esLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,75 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
+import prettier from 'eslint-config-prettier'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  {
+    ignores: ['dist']
+  },
+
+  js.configs.recommended,
+
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+
+    rules: {
+      // React
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+
+      // Hooks
+      ...reactHooks.configs.recommended.rules,
+
+      // Vite
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true }
+      ],
+
+      // Estilo general
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'never'],
+      'no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_'
+        }
+      ]
+    }
   },
-])
+
+  prettier
+]
